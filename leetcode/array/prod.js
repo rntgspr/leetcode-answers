97f0ce1f35d6c7f3f8562ed91ec19af3ee962973
rntgspr <rntgspr@gmail.com>
2024-05-05 18:41:01 -0700

    Adding files

---
+++ leetcode/array/prod.js
@@ -0,0 +1,63 @@
+/*
+Given an integer array nums, return an array answer such that answer[i] is equal to the product
+of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+Constraints:
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ 
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not
+  count as extra space for space complexity analysis.)
+*/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+// var productExceptSelf = function (nums) {
+//   let answer = [];
+//   for (let i in nums) {
+//     for (let j in nums) {
+//       if (j !== i) {
+//         answer[i] = (answer?.[i] ?? 1) * nums[j];
+//       }
+//     }
+//   }
+//   return answer;
+// };
+
+var productExceptSelf = function (nums) {
+  const n = nums.length;
+  let answer = Array(n).fill(1);
+
+  let prefix = 1;
+  for (let i = 0; i < n; i++) {
+    answer[i] = prefix;
+    prefix *= nums[i];
+  }
+
+  let suffix = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    answer[i] *= suffix;
+    suffix *= nums[i];
+  }
+
+  return answer;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4]));
+console.log(productExceptSelf([-1, 1, 0, -3, 3]));
